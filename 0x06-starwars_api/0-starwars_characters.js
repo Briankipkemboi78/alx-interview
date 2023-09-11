6b8bfc038d2ed82f9512db96f0ab2ce1f2d79c1b
Brian <bkipkemboi@watuafrica.co.ke>
2023-09-11 07:51:57 +0300

    Changed code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,23 +3,49 @@
   prints all characters of a Star Wars movie
   */
 
-const myArgs = process.argv.slice(2);
-const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/' + myArgs[0];
+  const request = require('request');
 
-request(url, async function (error, response, body) {
-  if (!error) {
-    const json = JSON.parse(body);
-    const endpoints = json.characters;
-    for (const endpoint of endpoints) {
-      await new Promise(function (resolve, reject) {
-        request(endpoint, function (error, response, body) {
-          if (!error) {
-            console.log(JSON.parse(body).name);
-            resolve();
+  const movieId = process.argv[2];
+  
+  if (!movieId) {
+    console.log('Please provide a Movie ID as the first argument.');
+    process.exit(1);
+  }
+  
+  // Define the URL for the Star Wars API films endpoint
+  const filmsUrl = 'https://swapi.dev/api/films/';
+  
+  // Function to fetch the character data for a specific movie
+  function getCharactersForMovie(movieId) {
+    // Make a request to the films endpoint to get film data
+    request(filmsUrl, { json: true }, (error, response, body) => {
+      if (error) {
+        console.error('Error fetching data:', error);
+        return;
+      }
+  
+      // Find the movie with the specified ID
+      const movie = body.results.find((film) => film.episode_id == movieId);
+  
+      if (!movie) {
+        console.log(`Movie with ID ${movieId} not found.`);
+        return;
+      }
+  
+      // Fetch and print the characters in the same order as the "characters" list
+      movie.characters.forEach((characterUrl) => {
+        request(characterUrl, { json: true }, (error, response, characterData) => {
+          if (error) {
+            console.error('Error fetching character data:', error);
+            return;
           }
+  
+          console.log(characterData.name);
         });
       });
-    }
+    });
   }
-});
+  
+  // Call the function to fetch characters for the specified movie
+  getCharactersForMovie(movieId);
+  
